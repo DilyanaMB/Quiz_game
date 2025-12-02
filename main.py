1c1169897d688746a539e73400157c26cc8e017d
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-02 09:06:16 +0200

    part1

---
+++ main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for q in question_data:
+    question = Question(q['text'], q['answer'])
+    question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
